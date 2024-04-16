b79a610799c24e1f50de8a2ec7d64a1b39775d7f
03hoho03 <ghwjd321@naver.com>
2024-04-16 22:35:15 +0900

    Feat: 회원가입 api 로직

---
+++ what_team_my_team/_services/mutations/useSignup.ts
@@ -0,0 +1,39 @@
+import axiosInstance from '@/_lib/axios'
+import { useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+
+interface SignupResponse {
+  user: {
+    name: string
+    student_num: string
+    id: number
+    image: string
+  }
+}
+interface SignupVariables {
+  studentNum: string
+  name: string
+}
+
+const signup = (data: SignupVariables) => {
+  const body = {
+    student_num: data.studentNum,
+    name: data.name,
+  }
+  console.log(body)
+  const response = axiosInstance
+    .post<SignupResponse>('/auth/github/finish', body)
+    .then(({ data }) => data)
+
+  return response
+}
+
+const useSignup = () => {
+  const { mutate } = useMutation<SignupResponse, AxiosError, SignupVariables>({
+    mutationFn: signup,
+  })
+
+  return { mutate }
+}
+
+export default useSignup
