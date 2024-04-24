55a02a9c10aea73bbd35272a57f9e676f876ee5f
03hoho03 <ghwjd321@naver.com>
2024-04-24 17:03:39 +0900

    Feat: 팀 생성 페이지

---
+++ what_team_my_team/app/teamAdd/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormContainer from './_components/FormContainer'
+
+const TeamAdd = () => {
+  return (
+    <div className="flex flex-col items-center w-full">
+      <FormContainer />
+    </div>
+  )
+}
+
+export default TeamAdd
