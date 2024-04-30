5490382f7401acc58186e8ad2740a3c915c5ffc5
03hoho03 <ghwjd321@naver.com>
2024-05-01 01:23:01 +0900

    Feat: mock 상태에 따른 BASE_URL 추가

--- what_team_my_team/_lib/axios.ts
+++ what_team_my_team/_lib/axios.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+const baseURL =
+  process.env.NEXT_PUBLIC_API_MOCKING === 'enabled'
+    ? process.env.NEXT_PUBLIC_MOCK_BASE_URL
+    : process.env.NEXT_PUBLIC_BASE_URL
 
 const axiosInstance = axios.create({
   baseURL,
