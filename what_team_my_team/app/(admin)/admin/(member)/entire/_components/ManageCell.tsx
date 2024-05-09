d570261aeb3ea506786b43aaecac32544d112190
03hoho03 <ghwjd321@naver.com>
2024-05-09 16:16:58 +0900

    Feat: 전체 목록 테이블 manageCell

---
+++ what_team_my_team/app/(admin)/admin/(member)/entire/_components/ManageCell.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from 'react'
+import ManageDropdown from './ManageDropdown'
+import { ManageTableCellProps } from '@/_types/table'
+
+const ManageCell = ({ getValue }: ManageTableCellProps) => {
+  const userId = getValue() as string
+
+  return (
+    <div className="flex justify-center">
+      <ManageDropdown userId={userId} />
+    </div>
+  )
+}
+
+export default ManageCell
