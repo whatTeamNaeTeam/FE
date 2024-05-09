66194bfc61430929d25c92ee1caa18b15ea12d5f
03hoho03 <ghwjd321@naver.com>
2024-05-09 15:09:40 +0900

    Feat: alertDialog

---
+++ what_team_my_team/_components/ui/AlertDialog/index.ts
@@ -0,0 +1,21 @@
+import AlertDialog from './Root'
+import AlertDialogTrigger from './Trigger'
+import AlertDialogPortal from './Portal'
+import AlertDialogOverlay from './Overlay'
+import AlertDialogContent from './Content'
+import AlertDialogTitle from './Title'
+import AlertDialogDescription from './Description'
+import AlertDialogCancel from './Cancel'
+import AlertDialogAction from './Action'
+
+export {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction,
+}
