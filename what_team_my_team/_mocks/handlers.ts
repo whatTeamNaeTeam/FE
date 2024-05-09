d7510b8d98d5e486ac1dbd631d72b50412c5d0c8
03hoho03 <ghwjd321@naver.com>
2024-05-09 15:56:53 +0900

    Feat: 전체목록 관련 handler 추가

--- what_team_my_team/_mocks/handlers.ts
+++ what_team_my_team/_mocks/handlers.ts
@@ -1,34 +1,20 @@
 import { HttpResponse, http } from 'msw'
-
-const data = [
-  {
-    id: 1,
-    name: '장호정',
-    student_num: '201812379',
-    created_at: new Date().toLocaleDateString(),
-  },
-  {
-    id: 2,
-    name: '강태원',
-    student_num: '201812321',
-    created_at: new Date().toLocaleDateString(),
-  },
-  {
-    id: 3,
-    name: '박준영',
-    student_num: '201912379',
-    created_at: new Date().toLocaleDateString(),
-  },
-]
+import { AssignData, EntireData } from './datas'
 
 export const handlers = [
   http.get(`*/admin/user/manage`, () => {
-    return HttpResponse.json(data)
+    return HttpResponse.json(AssignData)
   }),
   http.patch(`*/admin/user/manage/*`, () => {
     return HttpResponse.json()
   }),
   http.delete(`*/admin/user/manage/*`, () => {
     return HttpResponse.json()
+  }),
+  http.get(`*/admin/user/list`, () => {
+    return HttpResponse.json(EntireData)
+  }),
+  http.delete(`*/admin/user/list`, () => {
+    return HttpResponse.json({ success: true })
   }),
 ]
