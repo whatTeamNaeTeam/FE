2f3c9c7d004aee3a493373b3e3d59837578d09c9
03hoho03 <ghwjd321@naver.com>
2024-07-05 00:15:25 +0900

    Feat: 이미지 도메인 허용 추가

--- what_team_my_team/next.config.mjs
+++ what_team_my_team/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: [process.env.NEXT_REPUBLIC_ALLOWED_IMAGE_DOMAIN],
+  },
 }
 
 export default nextConfig
