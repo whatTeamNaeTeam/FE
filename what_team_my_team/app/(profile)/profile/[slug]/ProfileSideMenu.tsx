1e7b74eb4b8bd97604eb423432a383feb5758270
03hoho03 <ghwjd321@naver.com>
2024-07-05 01:01:48 +0900

    Design: 반응형 추가

--- what_team_my_team/app/(profile)/profile/[slug]/ProfileSideMenu.tsx
+++ what_team_my_team/app/(profile)/profile/[slug]/ProfileSideMenu.tsx
@@ -1,6 +1,6 @@
 const ProfileSideMenu = () => {
   return (
-    <aside className="flex flex-col w-full max-w-[240px]">
+    <aside className="hidden flex-col w-full max-w-[240px] md:flex">
       <div className="flex flex-col gap-8 mb-40">
         <span className="text-xl text-gray-6">프로필</span>
         <span className="text-xl text-gray-6">내 활동</span>
