ebd7a63154526e306aac13e66c502c9c035d9439
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-08 01:57:12 +0900

    Fix: 에러 해결용 suspense 추가

--- what_team_my_team/app/(auth)/oauth/callback/github/page.tsx
+++ what_team_my_team/app/(auth)/oauth/callback/github/page.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Callback from './_components/callback'
 import WaitingModal from '@/_components/WaitingModal'
 
 const page = () => {
   return (
-    <div>
-      <Callback />
-      <WaitingModal open={true} />
-    </div>
+    <Suspense fallback={<></>}>
+      <div>
+        <Callback />
+        <WaitingModal open={true} />
+      </div>
+    </Suspense>
   )
 }
 
