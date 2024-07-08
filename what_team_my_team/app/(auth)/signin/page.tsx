29b60740248a52b0a7db3711c774be56e6533e26
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-08 19:14:40 +0900

    Revert "Test"
    This reverts commit 45a0edb54896041c0aa5a72ffc1db93676ab6f60.

--- what_team_my_team/app/(auth)/signin/page.tsx
+++ what_team_my_team/app/(auth)/signin/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import LoginContainer from './_components/LoginContainer'
 
-const page = () => {
+const SigninPage = () => {
   return (
     <div className="flex justify-center items-center">
       <LoginContainer />
@@ -9,4 +9,4 @@ const page = () => {
   )
 }
 
-export default page
+export default SigninPage
