4d81dc67c3764b1718cfffc0929b22256506cadb
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-15 13:24:53 +0900

    Feat: logout api handler

---
+++ what_team_my_team/_services/queries/useLogout.ts
@@ -0,0 +1,26 @@
+import axiosInstance from '@/_lib/axios'
+import { useMutation } from '@tanstack/react-query'
+import { isLoggedInState, userState } from '@/_stores/atoms/user'
+import { useSetAtom } from 'jotai'
+
+const logoutApi = async () => {
+  const response = await axiosInstance.post('/auth/logout')
+
+  return response
+}
+
+const useLogout = () => {
+  const setUserState = useSetAtom(userState)
+  const setIsLoggedIn = useSetAtom(isLoggedInState)
+  const logoutMutation = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      setUserState(null)
+      setIsLoggedIn(false)
+    },
+  })
+
+  return logoutMutation
+}
+
+export default useLogout
