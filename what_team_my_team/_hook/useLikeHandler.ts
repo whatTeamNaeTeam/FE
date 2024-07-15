d78c97bf5e3ebaf5d0171a53e930998eeae3c458
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-15 14:16:44 +0900

    Fix: isLike 상태 추가

--- what_team_my_team/_hook/useLikeHandler.ts
+++ what_team_my_team/_hook/useLikeHandler.ts
@@ -3,6 +3,7 @@ import { LikeCamel } from '@/_types/project'
 import useLikeState, {
   LIKE_STATE_KEY,
 } from '@/_services/mutations/useLikeState'
+import { useEffect, useState } from 'react'
 
 interface UseLikeHandlerProps {
   projectId: number
@@ -12,6 +13,7 @@ interface UseLikeHandlerProps {
 const useLikeHandler = ({ projectId, version }: UseLikeHandlerProps) => {
   const queryClient = useQueryClient()
   const likeMutation = useLikeState()
+  const [isLike, setIsLike] = useState(false)
 
   const toggleLike = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation()
@@ -22,9 +24,13 @@ const useLikeHandler = ({ projectId, version }: UseLikeHandlerProps) => {
 
   const data = queryClient.getQueryData<LikeCamel>([LIKE_STATE_KEY, projectId])
 
+  useEffect(() => {
+    setIsLike(!!data?.isLike)
+  }, [data])
+
   return {
     toggleLike,
-    isLike: data?.isLike,
+    isLike: isLike,
   }
 }
 
