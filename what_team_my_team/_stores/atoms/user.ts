050bcf01ddacd18eaf605d2dd89418d4fa7424a0
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-15 14:15:01 +0900

    Feat: 타입 추가

--- what_team_my_team/_stores/atoms/user.ts
+++ what_team_my_team/_stores/atoms/user.ts
@@ -1,11 +1,17 @@
-import { User } from '@/_services/mutations/useSocialLogin'
+import { UserCamel } from '@/_services/mutations/useSocialLogin'
 import { atomWithStorage, createJSONStorage } from 'jotai/utils'
 
-const storage = createJSONStorage(() => sessionStorage)
-export const userState = atomWithStorage('userState', null, storage)
+const userStorage = createJSONStorage<UserCamel | null>(() => sessionStorage)
+const isLoggedInStorage = createJSONStorage<boolean>(() => sessionStorage)
+
+export const userState = atomWithStorage<UserCamel | null>(
+  'userState',
+  null,
+  userStorage,
+)
 
 export const isLoggedInState = atomWithStorage(
   'isLoggedInState',
   false,
-  storage,
+  isLoggedInStorage,
 )
