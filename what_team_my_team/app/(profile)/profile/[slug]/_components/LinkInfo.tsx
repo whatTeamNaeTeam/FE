8c607691bd8f29ed313bf03354c4a5427351e1b2
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-16 21:55:51 +0900

    Fix: urls가 null일 때 오류 해결

--- what_team_my_team/app/(profile)/profile/[slug]/_components/LinkInfo.tsx
+++ what_team_my_team/app/(profile)/profile/[slug]/_components/LinkInfo.tsx
@@ -1,26 +1,31 @@
 'use client'
 
 import LinkAvatar from '@/_components/LinkAvatar'
+import { Url } from '@/_services/queries/useUserProfile'
 import React from 'react'
 
 interface LinkInfoProps {
-  urls: { url: string }[]
+  urls: Url[] | null
 }
 
 const LinkInfo = ({ urls }: LinkInfoProps) => {
   return (
     <div>
       <h3 className="text-base mb-2">링크</h3>
-      <ul className="flex flex-col gap-2">
-        {urls.map(({ url }, idx) => (
-          <li key={`${url}-${idx}`} className="flex items-center gap-2">
-            <LinkAvatar size={'link'} url={url} />
-            <a href={url} className="text-sm ml-2">
-              {url}
-            </a>
-          </li>
-        ))}
-      </ul>
+      {urls ? (
+        <ul className="flex flex-col gap-2">
+          {urls.map(({ url }, idx) => (
+            <li key={`${url}-${idx}`} className="flex items-center gap-2">
+              <LinkAvatar size={'link'} url={url} />
+              <a href={url} className="text-sm ml-2">
+                {url}
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span className="text-sm">없음</span>
+      )}
     </div>
   )
 }
