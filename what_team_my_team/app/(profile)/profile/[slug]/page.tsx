050c36cf407faeb0690b54614ff8e2e9b0b7ea1a
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-16 21:57:49 +0900

    Fix: auth 정보가 없어 isOwner 정보가 제대로 반영되지  않던 버그 수정

--- what_team_my_team/app/(profile)/profile/[slug]/page.tsx
+++ what_team_my_team/app/(profile)/profile/[slug]/page.tsx
@@ -3,24 +3,37 @@ import ProfileContainer from './_components/ProfileContainer'
 import { getQueryClient } from '@/app/getQueryClient'
 import {
   USER_PROFILE_KEY,
-  getUserProfile,
+  UserProfileReturn,
 } from '@/_services/queries/useUserProfile'
 import { dehydrate } from '@tanstack/react-query'
 import Hydrate from '@/_lib/hydrate.client'
+import axios from 'axios'
+import { cookies } from 'next/headers'
 
 const ProfilePage = async ({ params }: { params: { slug: string } }) => {
   const userId = params.slug
+  const cookieStore = cookies()
+  const accessCookie = cookieStore.get('access')?.value
 
   const queryClient = getQueryClient()
-  await queryClient.prefetchQuery({
+  await queryClient.prefetchQuery<UserProfileReturn>({
     queryKey: [USER_PROFILE_KEY, userId],
-    queryFn: () => getUserProfile(userId),
+    queryFn: async () => {
+      const response = await axios.get(
+        `https://api.whatmeow.shop/api/user/profile/${userId}`,
+        {
+          headers: { Authorization: `Bearer ${accessCookie}` },
+        },
+      )
+
+      return response.data
+    },
   })
   const dehydratedState = dehydrate(queryClient)
 
   return (
     <Hydrate state={dehydratedState}>
-      <div className="w-full">
+      <div className="w-full px-2">
         <ProfileContainer userId={userId} />
       </div>
     </Hydrate>
