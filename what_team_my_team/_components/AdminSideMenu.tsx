af0f73c36a4ac5213b58a867e46b255428a24c69
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-07-24 01:01:04 +0900

    Feat: 팀 관리 항목 추가

--- what_team_my_team/_components/AdminSideMenu.tsx
+++ what_team_my_team/_components/AdminSideMenu.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react'
 import MemberManageAccordion from '../app/(admin)/admin/(member)/assign/_components/MemberManageAccordion'
+import TeamManageAccordion from '@/app/(admin)/admin/(team)/team/assign/_components/TeamManageAccordion'
 
 const AdminSideMenu = () => {
   return (
     <div className="h-screen w-[240px] flex-shrink-0 bg-gray-7">
       <MemberManageAccordion />
+      <TeamManageAccordion />
     </div>
   )
 }
