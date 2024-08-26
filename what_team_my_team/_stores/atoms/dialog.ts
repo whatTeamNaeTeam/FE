2b2283d93690c37fce50f7359b7e2a134d13f723
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-08-26 16:02:48 +0900

    Feat: 삭제 및 탈퇴 dialog atom 추가

--- what_team_my_team/_stores/atoms/dialog.ts
+++ what_team_my_team/_stores/atoms/dialog.ts
@@ -1,3 +1,5 @@
 import { atom } from 'jotai'
 
 export const applyDialogAtom = atom<boolean>(false)
+export const leaveTeamDialogAtom = atom<boolean>(false)
+export const deleteTeamDialogAtom = atom<boolean>(false)
