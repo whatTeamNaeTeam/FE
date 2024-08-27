10c31ec97ff23531bcf52518d868f2f58668e9b9
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-08-27 18:49:29 +0900

    Feat: 멤버 컴포넌트 스토리

---
+++ what_team_my_team/stories/MemberItem.stories.tsx
@@ -0,0 +1,20 @@
+import { convertSnakeToCamel } from '../_utils/convertSnakeToCamel'
+import type { Meta, StoryObj } from '@storybook/react'
+import MemberItem from '@/app/(project)/project/[slug]/manage/member/_components/MemberItem'
+import { myTeamDetailData } from '@/_mocks/datas'
+
+const meta = {
+  title: 'Components/MemberItem',
+  component: MemberItem,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof MemberItem>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: { memberData: convertSnakeToCamel(myTeamDetailData.members_info[0]) },
+}
