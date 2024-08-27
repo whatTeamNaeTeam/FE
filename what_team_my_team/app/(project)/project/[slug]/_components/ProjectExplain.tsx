2a5b221ef5ecdbd14fbd85fb3b11d27e34d35240
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-08-28 02:55:32 +0900

    Fix: MDEditor light 모드 적용

--- what_team_my_team/app/(project)/project/[slug]/_components/ProjectExplain.tsx
+++ what_team_my_team/app/(project)/project/[slug]/_components/ProjectExplain.tsx
@@ -11,7 +11,12 @@ const ProjectExplain = ({ explain }: ProjectExplainProps) => {
   return (
     <div>
       <h3 className="text-xl font-bold mb-2">프로젝트 설명</h3>
-      <MDEditor.Markdown source={explain} style={{ whiteSpace: 'pre-wrap' }} />
+      <div data-color-mode="light">
+        <MDEditor.Markdown
+          source={explain}
+          style={{ whiteSpace: 'pre-wrap' }}
+        />
+      </div>
     </div>
   )
 }
