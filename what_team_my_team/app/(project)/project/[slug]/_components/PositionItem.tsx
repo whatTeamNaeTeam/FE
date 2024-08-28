343dc268ee66560315820326478e89c3446c508f
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-08-28 14:01:03 +0900

    Refact: 타입 변경 적용 및 지원 modal 상태 로직 변경

--- what_team_my_team/app/(project)/project/[slug]/_components/PositionItem.tsx
+++ what_team_my_team/app/(project)/project/[slug]/_components/PositionItem.tsx
@@ -1,26 +1,30 @@
 'use client'
 
 import Button from '@/_components/ui/Button'
-import { Tech } from '@/_services/queries/useProjectDetail'
+import { Category } from '@/_services/queries/useProjectDetail'
 import { applyDialogAtom } from '@/_stores/atoms/dialog'
+import { selectedPositionIdAtom } from '@/_stores/atoms/position'
+import { ConvertSnakeToCamel } from '@/_utils/convertSnakeToCamel'
 import { useSetAtom } from 'jotai'
 import React from 'react'
 
 interface PositionItemProps {
-  position: Tech
+  position: ConvertSnakeToCamel<Category>
 }
 
 const PositionItem = ({ position }: PositionItemProps) => {
   const setOpen = useSetAtom(applyDialogAtom)
+  const setSelectedPositionId = useSetAtom(selectedPositionIdAtom)
 
   const handleDialogState = () => {
     setOpen(true)
+    setSelectedPositionId(position.id)
   }
 
   return (
     <div className="flex justify-between items-center">
       <div className="flex flex-col gap-1">
-        <span className="text-sm text-indigo-6">개발</span>
+        {/* <span className="text-sm text-indigo-6">개발</span> */}
         <span className="text-sm font-medium">{position.tech}</span>
         <div className="flex text-xs text-gray-6 gap-2">
           <span>현원 {position.currentNum}</span>
