b9cd2be128cdbedad761e18c9316b93a1fd02271
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-09-03 17:05:34 +0900

    Refact: 변경된 서비스 로직 적용

--- what_team_my_team/app/(project)/project/[slug]/_components/ProjectContainer.tsx
+++ what_team_my_team/app/(project)/project/[slug]/_components/ProjectContainer.tsx
@@ -1,34 +1,35 @@
 'use client'
 
 import React from 'react'
-import { useProjectDetail } from '@/_services/queries/useProjectDetail'
 import PositionList from './PositionList'
 import ProjectExplain from './ProjectExplain'
 import PrimaryInfo from './PrimaryInfo'
-import SideMenu from './SideMenu'
 import LinkList from './LinkList'
 import ApplyDialog from './ApplyDialog'
+import { ImageSection } from './ImageSection'
+import { useProjectDetail } from '@/_hook/queries/project/useProjectDetail'
 
 interface ProjectContainerProps {
   teamId: string
 }
 
 const ProjectContainer = ({ teamId }: ProjectContainerProps) => {
-  const { data, isLoading } = useProjectDetail({ teamId })
+  const { data } = useProjectDetail({ teamId })
+
+  if (!data) {
+    return <div className="">...로딩중</div>
+  }
 
   return (
     <div className="flex justify-between w-full max-w-[1048px] px-2">
       <ApplyDialog teamId={teamId} />
-      {isLoading && <div className="">...로딩중</div>}
-      {data && (
-        <div className="flex flex-col gap-8 w-full px-6 py-12 md:flex-1 md:w-[330px]">
-          <PrimaryInfo team={data} />
-          <PositionList positions={data.team.category} />
-          <ProjectExplain explain={data.team.explain} />
-          <LinkList urls={data.team.urls} />
-        </div>
-      )}
-      <SideMenu />
+      <div className="flex flex-col gap-8 w-full px-6 py-12 md:flex-1 md:w-[330px]">
+        <PrimaryInfo team={data.team} isOwner={data.isLeader} />
+        <PositionList positions={data.team.category} />
+        {data.team.imageUrl && <ImageSection imageList={data.team.imageUrl} />}
+        <ProjectExplain explain={data.team.explain} />
+        <LinkList urls={data.team.urls} />
+      </div>
     </div>
   )
 }
