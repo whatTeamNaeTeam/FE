cc3c7164abf239a1f753b3803ccc486f4732e5ca
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-09-03 13:36:17 +0900

    Refact: query key 분리

--- what_team_my_team/_hook/queries/project/useProjectDetail.ts
+++ what_team_my_team/_hook/queries/project/useProjectDetail.ts
@@ -1,3 +1,4 @@
+import { PROJECT_DETAIL_KEY } from '@/_constants/queryKey'
 import { getProjectDetailApi } from '@/_services/project'
 import {
   ConvertedGetProjectDetailReturn,
@@ -7,8 +8,6 @@ import { convertSnakeToCamel } from '@/_utils/convertSnakeToCamel'
 import { useQuery } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 
-export const PROJECT_DETAIL_KEY = 'project-detail'
-
 export function useProjectDetail({ teamId }: { teamId: string }) {
   const projectDetailQuery = useQuery<
     GetProjectDetailReturn,
@@ -16,7 +15,7 @@ export function useProjectDetail({ teamId }: { teamId: string }) {
     ConvertedGetProjectDetailReturn
   >({
     queryFn: () => getProjectDetailApi({ teamId }),
-    queryKey: [PROJECT_DETAIL_KEY, teamId],
+    queryKey: [...PROJECT_DETAIL_KEY, teamId],
     select: (data) => {
       const convertedData = convertSnakeToCamel(data)
 
