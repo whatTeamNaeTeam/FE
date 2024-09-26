34b9e5930b6a553d66e3c922dd9d8a2458ede561
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-09-26 22:18:25 +0900

    Refact: 로그인 상태 atom 삭제

--- what_team_my_team/_hook/mutations/auth/useSocialLogin.ts
+++ what_team_my_team/_hook/mutations/auth/useSocialLogin.ts
@@ -1,6 +1,6 @@
 import { socialLoginApi } from '@/_services/auth'
 import { SocialLoginApiReturn } from '@/_services/type'
-import { isLoggedInState, userState } from '@/_stores/atoms/user'
+import { userState } from '@/_stores/atoms/user'
 import { convertSnakeToCamel } from '@/_utils/convertSnakeToCamel'
 import { useMutation } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
@@ -8,13 +8,11 @@ import { useSetAtom } from 'jotai'
 
 export function useSocialLogin() {
   const setUserState = useSetAtom(userState)
-  const setIsLoggedIn = useSetAtom(isLoggedInState)
+
   const { mutate } = useMutation<SocialLoginApiReturn, AxiosError, string>({
     mutationFn: socialLoginApi,
     onSuccess: (data) => {
-      const convertedData = convertSnakeToCamel(data)
-      setIsLoggedIn(true)
-      setUserState(convertedData.user)
+      setUserState(convertSnakeToCamel(data).user)
     },
   })
 
