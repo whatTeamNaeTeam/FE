073613081e857e3fd24a6097d094cb1a0f1fe9c8
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-09-26 22:17:33 +0900

    Refact: login 상태 atom 삭제

--- what_team_my_team/_hook/mutations/auth/useLogout.ts
+++ what_team_my_team/_hook/mutations/auth/useLogout.ts
@@ -1,16 +1,15 @@
 import { logoutApi } from '@/_services/auth'
-import { isLoggedInState, userState } from '@/_stores/atoms/user'
+import { userState } from '@/_stores/atoms/user'
 import { useMutation } from '@tanstack/react-query'
 import { useSetAtom } from 'jotai'
 
 export function useLogout() {
   const setUserState = useSetAtom(userState)
-  const setIsLoggedIn = useSetAtom(isLoggedInState)
+
   const logoutMutation = useMutation({
     mutationFn: logoutApi,
     onSuccess: () => {
       setUserState(null)
-      setIsLoggedIn(false)
     },
   })
 
