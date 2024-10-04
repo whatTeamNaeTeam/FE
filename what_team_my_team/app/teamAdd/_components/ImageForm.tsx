f24a99c7951e3c62968a914dd5ab5933b9271cd4
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2024-10-04 18:12:42 +0900

    Feat: 이미지 드래그드랍 기능 추가

--- what_team_my_team/app/teamAdd/_components/ImageForm.tsx
+++ what_team_my_team/app/teamAdd/_components/ImageForm.tsx
@@ -4,14 +4,24 @@ import Button from '@/_components/ui/Button'
 import React, { useState } from 'react'
 import { Control, useController } from 'react-hook-form'
 import { TeamAddFormValueType } from './FormContainer'
+import { useDragDrop } from '@/_hook/useDragDrop'
 
 interface ImageFormProps {
   control: Control<TeamAddFormValueType>
 }
 
 const ImageForm = ({ control }: ImageFormProps) => {
+  const { handleDragStart, handleDragEnd, handleDrop, handleDragOver } =
+    useDragDrop()
   const [image, setImage] = useState<string | undefined>()
   const { field } = useController({ name: 'image', control })
+
+  const changeImageField = (file: File | FileList) => {
+    field.onChange(file)
+  }
+  const changeImagePreview = (imageUrl: string) => {
+    setImage(imageUrl)
+  }
 
   const handleChangeFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target
@@ -19,10 +29,17 @@ const ImageForm = ({ control }: ImageFormProps) => {
       return
     }
 
-    field.onChange(files)
+    changeImageField(files)
 
     const ImageUrl = URL.createObjectURL(files[0])
-    setImage(ImageUrl)
+    changeImagePreview(ImageUrl)
+  }
+
+  const handleDropAfter = (files: FileList) => {
+    changeImageField(files)
+
+    const ImageUrl = URL.createObjectURL(files[0])
+    changeImagePreview(ImageUrl)
   }
 
   return (
@@ -33,6 +50,10 @@ const ImageForm = ({ control }: ImageFormProps) => {
           <label
             htmlFor="imageInput"
             className="relative block border w-full h-full"
+            onDragEnter={handleDragStart}
+            onDragEnd={handleDragEnd}
+            onDrop={(event) => handleDrop(event, handleDropAfter)}
+            onDragOver={handleDragOver}
           >
             <input
               id="imageInput"
